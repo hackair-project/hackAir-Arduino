50d47f59f502d8d8e298842e3efa68e48ddc1023
Thanasis Georgiou <contact@thgeorgiou.com>
2016-11-21 23:24:27 +0200

    Shield - Firmware Initial Commit

---
+++ WiFi Shield/ESP Firmware/README.md
@@ -0,0 +1,43 @@
+# ESP Firmware for the Arduino WiFi Shield
+This directory houses the ESP firmware's source code along with the configuration landing page.
+
+## Description & Features
+The ESP microcontroller is responsible for the following things when using the hackAIR Arduino WiFi shield:
+
+* Manage network connections
+* Store AP keys
+* Send data received from the Arduino to the hackAIR servers
+* Handle WiFi configuration
+
+To handle the above responsibilities the firmware has been based on top of the
+[nodemcu-httpserver](https://github.com/marcoskirsch/nodemcu-httpserver) project. NodeMCU and nodemcu-httpserver were
+chosen due to the ability to program the firmware using the Lua language, which makes it very easy for beginners to
+modify and hack the firmware as they see fit.
+
+When it's powered on, the ESP checks if there are any known WiFi Access Points in range to connect. If nothing is
+available, the ESP will start broadcasting it's own SSID and accept connections from other devices. The user can use
+their phone to select a WiFi network for the Arduino to use and input it's password. When instructed by the Arduino, the
+ESP will use this WiFi network to transmit gathered data.
+ 
+List of features:
+* Full featured HTTP server in AP mode.
+* Easy-to-use landing page for configuring WiFi connections
+* Handle all network stack duties, leaving the Arduino free to interface with external devices.
+* Power saving mode for extending battery life (WIP)
+
+## Install/Use
+For users looking to install the firmware to their devices there are tutorials and pre-built images available.
+For developers looking to modify the firmware, the development stack is based on Node.JS and the node package manager
+(npm). From a terminal at this directory type:
+
+    npm install
+    
+All required packages for developing will be installed. nodemcu-tool is used to manage writing files to the module.
+A couple of npm scripts are available to aid in development:
+ * `npm run upload`: Upload everything to the ESP module.
+ * `npm run format`: Format the ESP's filesystem.
+ * `npm run terminal`: Open a terminal to the ESP module.
+ 
+If uploading is unstable you can try lowering the baud rate at `.nodemcutool`. 9600 is usually a safe bet but it's really slow.
+ 
+## License
---
+++ WiFi Shield/ESP Firmware/src/init.lua
@@ -0,0 +1,81 @@
+--- hackAIR ESP Firmware
+-- Handles WiFi setup and network communications
+-- @version 0.1.0
+-- @author Thanasis Georgiou
+
+--- The server URL used for sending data to the hackAIR project
+server_URL = "http://hackair.draxis.gr:8000/v1/sensors/arduino/measurements"
+auth_token = ""
+
+function send_to_hackAIR(args)
+    -- Parse args
+    local pm25, pm10, battery, tamper, error = string.match(args, "([^,]+),([^,]+),([^,]+),([^,]+),([^,]*)")
+    local requestJson = '{"reading":{"PM2.5_AirPollutantValue":"' .. pm25 .. '","PM10_AirPollutantValue":"' .. pm10 ..'"},"battery":"' .. battery ..'","tamper":"' .. tamper ..'","error":"' .. error .. '"}'
+    http.post(server_URL, 'Content-Type: application/json\r\nAuthorization: '..auth_token..'\r\n', requestJson,
+        function(code, data)
+            uart.write(0, tostring(code))
+        end
+    )
+end
+
+--- Handle UART commands
+-- The first word (string until first space) is the command, the rest is
+-- parameters. Each command is terminated with a \n.
+-- @param UART input, must be parsed
+function handle_uart(data)
+    -- Parse parameters
+    local com, args = string.match(data, "([^%s]+)%s([^%s]+)")
+    -- print("command: "..com)
+    -- if args ~= nil then print("args: "..args) end
+    
+    -- Execute commands
+    if com == 'e+raw' then -- Enter raw mode (lua console)
+        uart.on("data")
+        uart.setup(0, 115200, 8, uart.PARITY_NONE, uart.STOPBITS_1, 1)
+    elseif com == 'e+restart' then -- Restart ESP
+        node.restart()
+    elseif com == 'e+send' then -- Send data to server
+        send_to_hackAIR(args)
+    elseif com == 'e+clearap' then -- Clear saved access points
+        wifi.sta.config('', '')
+        node.restart()
+    elseif com == 'e+token' then -- Grab the token for authorization
+	    auth_token = args
+    elseif com == 'e+isready' then -- Is the ESP ready and connected?
+        if wifi.sta.getip() ~= nil then
+            uart.write(0, tostring(1))
+        else
+            uart.write(0, tostring(0))
+        end
+    end
+end
+
+-- Start listening to UART for commands
+uart.setup(0, 115200, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
+uart.on('data', "\n", handle_uart, 0)
+
+-- Create AP for configuration
+wifi.ap.config({ssid = "hackAIR-"..node.chipid(), pwd = "hackAIR-"..node.chipid()})
+wifi.setmode(wifi.STATIONAP)
+
+-- Manual mode
+enduser_setup.manual(true)
+enduser_setup.start(
+    function()
+        -- WiFi information is stored in wifi.sta.config so we don't have
+        -- to do anything special here.
+        
+        -- Print debug info
+        print("Connected to wifi as:" .. wifi.sta.getip())
+ 
+        -- Stop portal
+        enduser_setup.stop()
+        wifi.setmode(wifi.NULLMODE)
+        wifi.setmode(wifi.STATION)
+        wifi.sta.connect()
+    end,
+    function(err, str)
+        print("enduser_setup: Err #" .. err .. ": " .. str)
+    end)
+
+wifiConfig = nil
