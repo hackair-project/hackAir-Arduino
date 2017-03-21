1a037a8facde1fce1d3ba9d00bdc8f4d38f580b2
Thanasis Georgiou <contact@thgeorgiou.com>
2017-03-21 20:15:51 +0200

    Shield - Display welcome message to make it clear why the unit is not responding to lua commands

--- WiFi Shield/ESP Firmware/src/init.lua
+++ WiFi Shield/ESP Firmware/src/init.lua
@@ -81,6 +81,10 @@ function handle_uart(data)
     end
 end
 
+-- Display a warning message to the prompt
+print('hackAir ESP Firmware')
+print('Entering serial command mode, to use the Lua terminal send "e+raw"')
+
 -- Start listening to UART for commands
 uart.setup(0, 115200, 8, uart.PARITY_NONE, uart.STOPBITS_1, 0)
 uart.on('data', "\n", handle_uart, 0)
